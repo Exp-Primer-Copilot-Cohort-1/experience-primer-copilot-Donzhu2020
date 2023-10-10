82dcc1c4b2b684968af3f2ec2138d41ef919d00a
Donzhu2020 <79619870+Donzhu2020@users.noreply.github.com>
2023-10-10 02:57:41 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,30 @@
+// Create web server
+// 1. npm install express --save
+// 2. npm install body-parser --save
+// 3. npm install cors --save
+// 4. npm install mongoose --save
+// 5. npm install moment --save
+// 6. npm install nodemon --save-dev
+// 7. npm install bcrypt-nodejs --save
+// 8. npm install jsonwebtoken --save
+// 9. npm install multer --save
+// 10. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 11. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 12. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 13. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 14. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 15. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 16. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 17. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 18. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 19. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+
+// 20. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 21. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 22. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 23. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 24. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 25. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 26. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 27. npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// 28. npm install --
